47c67dd091df93d1fbdcad3ef984588cec5499ba
scrappy1987 <jagordon87@msn.com>
2018-07-17 15:26:34 +0100

    inital commit

---
+++ src/main/java/com/qa/Receiver.java
@@ -0,0 +1,12 @@
+package com.qa;
+
+import java.util.concurrent.CountDownLatch;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+    }
+}
